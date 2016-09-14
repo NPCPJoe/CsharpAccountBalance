c0ecdd25669b31b138a04efcda8c54fef431ace2
NPCPJoe <joecoo93@gmail.com>
2016-09-14 12:25:34 +0100

    code

---
+++ Account balance/Account balance/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Account_balance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RBDeposit = new System.Windows.Forms.RadioButton();
+            this.RBCheck = new System.Windows.Forms.RadioButton();
+            this.RBServicecharge = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBTransactionamount = new System.Windows.Forms.TextBox();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnSummary = new System.Windows.Forms.Button();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.TBNewaccountbalance = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // RBDeposit
+            // 
+            this.RBDeposit.AutoSize = true;
+            this.RBDeposit.Location = new System.Drawing.Point(22, 19);
+            this.RBDeposit.Name = "RBDeposit";
+            this.RBDeposit.Size = new System.Drawing.Size(77, 21);
+            this.RBDeposit.TabIndex = 0;
+            this.RBDeposit.TabStop = true;
+            this.RBDeposit.Text = "Deposit";
+            this.RBDeposit.UseVisualStyleBackColor = true;
+            // 
+            // RBCheck
+            // 
+            this.RBCheck.AutoSize = true;
+            this.RBCheck.Location = new System.Drawing.Point(22, 46);
+            this.RBCheck.Name = "RBCheck";
+            this.RBCheck.Size = new System.Drawing.Size(68, 21);
+            this.RBCheck.TabIndex = 1;
+            this.RBCheck.TabStop = true;
+            this.RBCheck.Text = "Check";
+            this.RBCheck.UseVisualStyleBackColor = true;
+            // 
+            // RBServicecharge
+            // 
+            this.RBServicecharge.AutoSize = true;
+            this.RBServicecharge.Location = new System.Drawing.Point(22, 73);
+            this.RBServicecharge.Name = "RBServicecharge";
+            this.RBServicecharge.Size = new System.Drawing.Size(126, 21);
+            this.RBServicecharge.TabIndex = 2;
+            this.RBServicecharge.TabStop = true;
+            this.RBServicecharge.Text = "Service Charge";
+            this.RBServicecharge.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Transaction Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "New Account Balance";
+            // 
+            // TBTransactionamount
+            // 
+            this.TBTransactionamount.Location = new System.Drawing.Point(403, 27);
+            this.TBTransactionamount.Name = "TBTransactionamount";
+            this.TBTransactionamount.Size = new System.Drawing.Size(143, 22);
+            this.TBTransactionamount.TabIndex = 5;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(168, 161);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(120, 48);
+            this.BtnClear.TabIndex = 6;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(306, 161);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(120, 48);
+            this.BtnExit.TabIndex = 7;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // BtnSummary
+            // 
+            this.BtnSummary.Location = new System.Drawing.Point(446, 161);
+            this.BtnSummary.Name = "BtnSummary";
+            this.BtnSummary.Size = new System.Drawing.Size(120, 48);
+            this.BtnSummary.TabIndex = 8;
+            this.BtnSummary.Text = "Summary";
+            this.BtnSummary.UseVisualStyleBackColor = true;
+            this.BtnSummary.Click += new System.EventHandler(this.BtnSummary_Click);
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(33, 161);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(120, 48);
+            this.BtnCalculate.TabIndex = 9;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // TBNewaccountbalance
+            // 
+            this.TBNewaccountbalance.Enabled = false;
+            this.TBNewaccountbalance.Location = new System.Drawing.Point(403, 73);
+            this.TBNewaccountbalance.Name = "TBNewaccountbalance";
+            this.TBNewaccountbalance.Size = new System.Drawing.Size(143, 22);
+            this.TBNewaccountbalance.TabIndex = 10;
+            this.TBNewaccountbalance.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 221);
+            this.Controls.Add(this.TBNewaccountbalance);
+            this.Controls.Add(this.BtnCalculate);
+            this.Controls.Add(this.BtnSummary);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.TBTransactionamount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RBServicecharge);
+            this.Controls.Add(this.RBCheck);
+            this.Controls.Add(this.RBDeposit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton RBDeposit;
+        private System.Windows.Forms.RadioButton RBCheck;
+        private System.Windows.Forms.RadioButton RBServicecharge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBTransactionamount;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button BtnSummary;
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.TextBox TBNewaccountbalance;
+    }
+}
+
---
+++ Account balance/Account balance/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_balance
+{
+    public partial class Form1 : Form
+    {
+        private static double balance = 0;
+        private static int totalDeposits = 0; // used for summary of all deposits
+        private static double totalDepositsAmnt = 0; // used for summary of all deposits dollar amount
+        private static int totalChecks = 0; // ued for summar of all checks
+        private static double totalCheckAmnt = 0; // used for summary of all checks dollar amounts
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            TBTransactionamount.Clear();
+            TBNewaccountbalance.Clear();
+            RBCheck.Checked = false;
+            RBDeposit.Checked = false;
+            RBServicecharge.Checked = false;
+            
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            char transaction = ' '; //What transaction we are performing (deposit, check, service charge)
+            // find out what radio button is selected.
+            if (RBDeposit.Checked)
+                transaction = 'd'; //depoist
+            else if (RBServicecharge.Checked)
+                transaction = 's'; //service charge
+            else if (RBCheck.Checked)
+                transaction -= 'c'; //check
+
+            switch(transaction)
+            {
+                case 'd':
+                    if (validateTextBox())// validate if user entered valid number into the text box.
+                    {
+                        double depoist = Convert.ToDouble(TBTransactionamount.Text);
+                        depoistTransaction(depoist);
+                        displatBalance();
+                    }
+                    else
+                        errormessage();
+                    break;
+                        case 's':
+                    if (validateTextBox())
+                    {
+                        double serviceCharge = Convert.ToDouble(TBTransactionamount.Text);
+                        serviceChargeTransaction(serviceCharge);
+                        displatBalance();
+                    }
+                    else
+                        errormessage();
+                    break;
+                case 'c':
+                    if (validateTextBox())
+                    {
+                        double check = Convert.ToDouble(TBTransactionamount.Text);
+                        checkTransaction(check);
+                        displatBalance();
+                    }
+                    else
+                        errormessage();
+                    break;
+                default:
+                    MessageBox.Show("select a transaction");
+                    break;
+
+            }
+        }
+        private void checkTransaction(double c)
+        {
+            if (balance - c >=0)
+            {
+                balance -= c;
+                totalChecks += 1;
+                totalCheckAmnt += c;
+            }
+            else
+            {
+                balance -= 10;
+                totalCheckAmnt += 10;
+                MessageBox.Show("Insufficent funds");
+            }
+        }
+        private void serviceChargeTransaction(double s)
+        {
+            if (balance - s >= 0)
+            {
+                balance -= s;
+            }
+            else
+                MessageBox.Show("The transaction cannot be completed because the balance would be less than zero");
+        }
+        private void errormessage()//displays error message if a valid amount has not been entered.
+        {
+            MessageBox.Show("Invalid entry. Please select a valid amount.");
+        }
+        private void displatBalance() // display balance in text box
+        {
+            TBNewaccountbalance.Text = "$" + balance.ToString();
+        }
+        private void depoistTransaction(double d)//performs depoist and adds to the curent total
+        {
+            balance += d;
+            totalDeposits += 1;
+            totalDepositsAmnt += d;
+        }
+        private bool validateTextBox()//return true if user enters valid number
+        {
+            double value;
+            return double.TryParse(TBTransactionamount.Text, out value);// pulls the amount the users enters. if the value is able to be a double then it is true. 
+
+        }
+
+        private void BtnSummary_Click(object sender, EventArgs e)
+        {
+            string message = "total number of depoists:" + totalDeposits;
+            message += "\n Total amount of depoists: $" + totalDepositsAmnt;
+            message += "\n Total amount of checks:" + totalChecks;
+            message += "\n Total amount of checks: " + totalCheckAmnt;
+
+            MessageBox.Show(message);
+
+
+        }
+    }
+}
